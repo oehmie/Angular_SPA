092d4a29180b1bf9e881d194cb6fd515ef8da634
oehmie <markus@oehmie.de>
2014-04-25 07:29:50 +0200

    Klassen vergessen

---
+++ Domain/Classes/PagedResponse.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Domain.Classes {
+   public class PagedResponse<T> {
+
+      public PagedResponse(IEnumerable<T> data, int totalRows)
+        {
+            Data = data;
+            TotalRows = totalRows;
+        }
+        public int TotalRows { get; set; }
+        public IEnumerable<T> Data { get; set; }
+   }
+}
